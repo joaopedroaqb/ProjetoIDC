00993da2e0d68393ee21adf9bb60cd3c9a1f1879
João Pedro Araujo Queiroz Barbosa <111623525+joaopedroaqb@users.noreply.github.com>
2023-11-29 00:30:46 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,21 @@ const arduino = new SerialPort({ path: 'COM1' });
 function controlarLED(estado) {
     arduino.write(estado); 
 }
+
+//Ou tentar rodar com esse
+/*$(document).ready(function() {
+    $("#ligarBtn").click(function() {
+        controlarLED('ON');
+    });
+
+    $("#desligarBtn").click(function() {
+        controlarLED('OFF');
+    });
+
+    function controlarLED(estado) {
+        $.get('/?LED=' + estado, function(data) {
+            // Atualiza o status com base na resposta do servidor
+            $("#status").text("Status: " + data);
+        });
+    }
+});
